906eae661ec34ce5209f9d48b8eb375c3a2bcc0f
Basel Al Nassr <mrbasel555@gmail.com>
2020-05-30 20:23:10 +0300

    Added firestores's article stream

--- pocket_clone/lib/main.dart
+++ pocket_clone/lib/main.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+// import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_html/flutter_html.dart';
@@ -16,6 +17,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
+class ArticlesStream extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return StreamBuilder(
+        stream: Firestore.instance.collection('articles').snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (!snapshot.hasData) return CircularProgressIndicator();
+          return ArticlesListView(documents: snapshot.data.documents);
+        }
+        );
+  }
+}
+
+class ArticlesListView extends StatelessWidget {
+  final List<DocumentSnapshot> documents;
+
+  ArticlesListView({this.documents});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: ScrollPhysics(),
+      itemCount: documents.length,
+      itemBuilder: (BuildContext context, int index){
+        String title = documents[index].data['title'].toString();
+        String website = documents[index].data['website'].toString();
+        
+        return ArticleListItem(title, website);
+      }
+      );
+  }
+}
+
+
 class HomePage extends StatefulWidget {
   @override
   _HomePageState createState() => _HomePageState();
@@ -32,18 +68,18 @@ class _HomePageState extends State<HomePage> {
   return data;   
 }
 
-  renderArticle(){
-    var data;
-    if (article != null){
-      data = article['title'];
-    }   
-    else{
-      data = '';
-    }
-    return Html(
-        data: data
-        );
-  }
+  // renderArticle(){
+  //   var data;
+  //   if (article != null){
+  //     data = article['title'];
+  //   }   
+  //   else{
+  //     data = '';
+  //   }
+  //   return Html(
+  //       data: data
+  //       );
+  // }
 
   @override
   Widget build(BuildContext context) {
@@ -53,7 +89,7 @@ class _HomePageState extends State<HomePage> {
         title: Text('My app!'),
         backgroundColor: Colors.deepOrange,
       ),
-      body: 
+      body:
         ListView(
         children: <Widget>[
           Container(
@@ -80,7 +116,6 @@ class _HomePageState extends State<HomePage> {
           ),
           ),
           
-          renderArticle(),
           Container(
             child: Text('Saved Articles',
           style: TextStyle(
@@ -90,10 +125,15 @@ class _HomePageState extends State<HomePage> {
           ),
           margin: EdgeInsets.only(top: 20, bottom: 15),
           ),
-        ],
-      ),
+
+          // List of saved articles
+          ArticlesStream()
+          ],
+      ), 
+          
+          );
  
-    );
+    
   }
 }
 
